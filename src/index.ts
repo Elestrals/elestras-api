fc49c39ae71d8bffcf9203220fbe42da77db2a49
aadriance <primaryartemis@outlook.com>
2025-06-13 13:08:49 -0700

    repo : bun project init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Elysia } from "elysia";
+
+new Elysia()
+    .get("/", "Hello Elysia")
+    .get("user/:id", ({ params: { id }}) => id)
+    .post("/form", ({ body }) => body)
+    .get("/card/:id", ({ params: { id }}) => Bun.file(`data/cards/${id}.json`))
+    .listen(3000);
